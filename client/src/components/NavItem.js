5612b5dffa27a530f9fa242820ffbc920adb8d64
Dmitriy Prokoshev <pda1990@mail.ru>
2021-03-18 22:53:32 +0300

    fix: warning parentExpanded (deleted from props in NavItem)

--- client/src/components/NavItem.js
+++ client/src/components/NavItem.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Navigation } from '@8base/boost';
 import { NavLink } from 'react-router-dom';
 
-const NavItem = props => <Navigation.Item tagName={NavLink} {...props} />;
+const NavItem = props => {
+  const {parentExpanded, ...rest} = props;
+  return <Navigation.Item tagName={NavLink} {...rest} />;
+}
 
 export { NavItem };
